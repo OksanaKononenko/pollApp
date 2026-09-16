02e8b0d63f6ab0b096ef6cd9e181b936457c113a
OksanaKononenko <ksena_ua2025@ukr.net>
2026-09-16 15:42:21 +0300

    add hash location strategy for angular routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
  import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http'; // НОВЕ: Імпортуємо налаштування для HTTP-запитів
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient() // НОВЕ: Вмикаємо можливість робити мережеві запити
   ]
 };
